bbfce905bc603d1c53bf50e0135ec76df416e5da
AhmedMiDo75 <codetunefx@gmail.com>
2025-04-10 13:05:08 +0200

    project-finished

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -23,6 +23,11 @@ function onAddItemSubmit(e) {
     itemToEdit.classList.remove("edit-mode");
     itemToEdit.remove();
     isEditMode = false;
+  } else {
+    if (checkIfItemExists(newItem)) {
+      alert(`That item already exists!`);
+      return;
+    }
   }
   // Create Item DOM Element
   addItemToDOM(newItem);
@@ -87,6 +92,11 @@ function onClickItem(e) {
   } else {
     setItemToEdit(e.target);
   }
+}
+
+function checkIfItemExists(item) {
+  const itemsFromStorage = getItemsFromStorage();
+  return itemsFromStorage.includes(item);
 }
 
 function setItemToEdit(item) {
